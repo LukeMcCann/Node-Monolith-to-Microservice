ad94db41aeedd32b1ad45b86e9911c787671b4d7
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-08 21:28:20 +0000

    Return database data

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     databaseFunction();
-    res.status(200).send('Hello World');
+    res.status(200).send(databaseFunction());
 });
 
 app.listen(port, () => {
