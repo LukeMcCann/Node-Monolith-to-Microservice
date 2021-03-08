cbfa200dda24767c9ccd689a381a273968420a3e
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-08 21:17:44 +0000

    Add hello world monolith

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 const app = require('express')();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+});
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
